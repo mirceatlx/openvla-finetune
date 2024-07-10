2c4b317a53b83378f978fc3a566f34240b36c6fb
mirceatlx <mirceatlx@gmail.com>
2024-07-10 10:45:47 +0200

    init soft

---
+++ dataset.py
@@ -0,0 +1,85 @@
+from torch.utils.data import Dataset
+import tensorflow_datasets as tfds
+
+
+class SoftDataset(tfds.core.GeneratorBasedBuilder):
+    """
+    DatasetBuilder for the Soft Robot dataset.
+    """
+
+    VERSION = tdfs.core.Version("1.0.0")
+    RELEASE_NOTES = {
+        "1.0.0": "Initial release."
+    }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _info(self) -> tfds.core.DatasetInfo:
+        return tfds.core.DatasetInfo(
+            features=tfds.features.FeaturesDict({
+                'steps': tfds.features.Dataset({
+                    'observation': tfds.features.Image(shape=(224, 224, 3), dtype=tf.uint8, encoding_format='png'),
+                    'action': tfds.features.Tensor(shape=(7,), dtype=tf.float32),
+                    'is_first': tfds.features.Tensor(shape=(), dtype=tf.bool),
+                    'is_last': tfds.features.Tensor(shape=(), dtype=tf.bool),
+                    'metadata': tfds.features.Text(),
+                }),
+                'metadata': tfds.features.Text(),
+                'prompt': tfds.features.Text(),
+            }
+        )
+
+class DummyDataset(Dataset):
+    def __init__(
+        self,
+        action_tokenizer: ActionTokenizer,
+        base_tokenizer: PreTrainedTokenizerBase,
+        image_transform: ImageTransform,
+        prompt_builder_fn: Type[PromptBuilder],
+    ) -> None:
+        self.action_tokenizer = action_tokenizer
+        self.base_tokenizer = base_tokenizer
+        self.image_transform = image_transform
+        self.prompt_builder_fn = prompt_builder_fn
+
+        # Note =>> We expect the dataset to store statistics for action de-normalization. Specifically, we store the
+        # per-dimension 1st and 99th action quantile. The values below correspond to "no normalization" for simplicity.
+        self.dataset_statistics = {
+            "dummy_dataset": {
+                "action": {"q01": np.zeros((7,), dtype=np.float32), "q99": np.ones((7,), dtype=np.float32)}
+            }
+        }
+
+    def __len__(self):
+        # TODO =>> Replace with number of elements in your dataset!
+        return 10000
+
+    def __getitem__(self, idx):
+        # TODO =>> Load image, action and instruction from disk -- we use dummy values
+        image = Image.fromarray(np.asarray(np.random.rand(224, 224, 3) * 255.0, dtype=np.uint8))
+        action = np.asarray(np.random.rand(7), dtype=np.float32)
+        instruction = "do something spectacular"
+
+        # Add instruction to VLA prompt
+        prompt_builder = self.prompt_builder_fn("openvla")
+        conversation = [
+            {"from": "human", "value": f"What action should the robot take to {instruction}?"},
+            {"from": "gpt", "value": self.action_tokenizer(action)},
+        ]
+        for turn in conversation:
+            prompt_builder.add_turn(turn["from"], turn["value"])
+
+        # Tokenize (w/ `base_tokenizer`)
+        input_ids = self.base_tokenizer(prompt_builder.get_prompt(), add_special_tokens=True).input_ids
+        labels = list(input_ids)
+
+        # Tensorize =>> Run Image Transform to get `pixel_values` =>> Return
+        #   =>> IMPORTANT :: IF WE'RE USING HF .forward(..., labels=labels), SHIFTING HAPPENS _INSIDE_ MODEL!
+        input_ids, labels = torch.tensor(input_ids), torch.tensor(labels)
+        pixel_values = self.image_transform(image)
+
+        # [CRITICAL] We do not want to take the loss for anything but the predicted action tokens!
+        labels[: -(len(action) + 1)] = IGNORE_INDEX
+
+        return dict(pixel_values=pixel_values, input_ids=input_ids, labels=labels)
---
+++ transforms.py
@@ -0,0 +1,8 @@
+"""
+Define transformations to your dataset to adhere to the requirements for OpenVLA. 
+"""
+
+
+
+def soft_robot_transform(data: dict) -> dict:
+    pass
